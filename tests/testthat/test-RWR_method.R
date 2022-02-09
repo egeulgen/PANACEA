e3d3eb9eb310051fcf37670e1eaa9213ef032711
egeulgen <egeulgen@gmail.com>
2022-02-09 15:16:07 +0300

    smaller test for RWR

--- tests/testthat/test-RWR_method.R
+++ tests/testthat/test-RWR_method.R
@@ -1,6 +1,6 @@
 test_that("RWR scoring works", {
     skip_on_cran()
-    W_mat <- PANACEA:::adj_list2mat(STRING_adj_df[STRING_adj_df$combined_score > 900, ])
+    W_mat <- PANACEA:::adj_list2mat(STRING_adj_df[STRING_adj_df$combined_score > 990, ])
     selected_drugs <- unique(DGIdb_interactions_df$drug_name)[1:20]
     sel_interactions_df <- DGIdb_interactions_df[DGIdb_interactions_df$drug_name %in% selected_drugs, ]
     expect_type(score_drugs_RWR_based(driveR_res = example_driveR_res,
